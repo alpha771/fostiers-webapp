0defa6711edd7c56a0322ac0a7a847c4581eb2c8
alpha771 <youseffislamm@gmail.com>
2021-01-04 16:34:55 +0700

    Style: remove semicolons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const Login = lazy(() => import('./pages/login'))
